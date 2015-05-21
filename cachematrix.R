70361d132e1279b4eb6cfde22bc5439a6401828f
claudio <bragaclaudio@ig.com.br>
2015-05-21 15:42:21 -0300

    Add comment in makeCacheMatrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,12 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function do a cache matrix.
+## It has 4 options:
+## You have inform a matrix when you call "makeCacheMatrix".
+## The function will store the given matrix.
+## You can change the matrix with the function "set"
+## You can recover the stored matrix with the function "get"
+## You can set the inverse matrix with the function "setInverse"
+## You can recover the inverse matrix with the function "getInverse"
+## It will return NULL if the function "cacheSolve" and "setInverse" have never been used.
 
 makeCacheMatrix <- function(x = matrix()) { ## Function makeCacheMatrix
   m <- NULL
